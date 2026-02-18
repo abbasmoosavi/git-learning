c2c5cd2bf378bee151cc69e9ba03949d422df093
moosaviabbas93@gmail.com <moosaviabbas93@gmail.com>
2026-02-19 00:04:10 +0330

    feat(main): Added Abbas.tsx file

---
+++ Abbas.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Abbas = () => {
+  return (
+    <View>
+      <Text>Abbas</Text>
+    </View>
+  )
+}
+
+export default Abbas
