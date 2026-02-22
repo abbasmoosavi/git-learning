e5360f4e0fab495f1198061819fa5975f7f11b5c
moosaviabbas93@gmail.com <moosaviabbas93@gmail.com>
2026-02-22 23:14:21 +0330

    Added Ali.tsx file

---
+++ Ali.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Ali = () => {
+  return (
+    <View>
+      <Text>Ali</Text>
+    </View>
+  );
+};
+
+export default Ali;
